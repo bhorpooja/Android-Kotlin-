09243264121f52ac6b5dabd7f2ca80755b62d646
pooja <bhorpooja222gmail.com>
2017-11-28 22:17:01 +0530

    Sqlite using Java

---
+++ sqlitejava/src/main/java/com/codekul/sqlitejava/db/AppDb.java
@@ -0,0 +1,15 @@
+package com.codekul.sqlitejava.db;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.RoomDatabase;
+
+import com.codekul.sqlitejava.dao.CarDao;
+import com.codekul.sqlitejava.model.Car;
+/**
+ * Created by pooja on 28/11/17.
+ */
+
+@Database(entities = {Car.class}, version = 1)
+public abstract class AppDb extends RoomDatabase{
+    public abstract CarDao carDao();
+}
