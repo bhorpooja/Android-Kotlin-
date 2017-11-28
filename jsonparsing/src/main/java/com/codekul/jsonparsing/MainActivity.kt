8f9e9ae19e0d21b371b1a92fe036503442603c25
pooja <bhorpooja222gmail.com>
2017-11-28 17:10:36 +0530

    json parsing using gson

--- jsonparsing/src/main/java/com/codekul/jsonparsing/MainActivity.kt
+++ jsonparsing/src/main/java/com/codekul/jsonparsing/MainActivity.kt
@@ -3,6 +3,8 @@ package com.codekul.jsonparsing
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.codekul.jsonparsing.dto.Demo
+import com.google.gson.Gson
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.InputStream
@@ -13,6 +15,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        gson()
+    }
+
+    fun gson(){
+
+        val ist: InputStream = assets.open("my.json")
+        val flDt = ist.bufferedReader().use {
+            it.readText()
+        }
+
+
+        val gs=Gson()
+        val demo=gs.fromJson<Demo>(flDt,Demo::class.java)
+        Log.i("@Codekul", """Name : ${demo.nm}""")
+        Log.i("@Codekul", """Age : ${demo.age}""")
+        Log.i("@Codekul", """isIndian : ${demo.isIndian}""")
+        demo.mobs.forEach {
+            Log.i("@codekul","""$it""")
+        }
+
+
+    }
+
+    fun normal(){
 
         val ist: InputStream = assets.open("my.json")
         val flDt = ist.bufferedReader().use {
@@ -35,7 +61,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private operator fun JSONArray.iterator(): Iterator<String>
             = (0 until length()).asSequence().map { get(it) as String }.iterator()
+
 }
